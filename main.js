ce9ac5df043492823e348eca1f6a924f252bf83d
jaint4 <75832832+jaint4@users.noreply.github.com>
2020-12-30 19:16:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function getparagraph1(){
+    var input=[];
+}
+for (var i=1; i<=6 ; ++i)
+{
+    inputs.push(document.getElementById("para1_input_box" + i).value);
+}
+inputs.join(" .")
+document.getElementById("showpragraph1").innerHTML=inputs.join(" .")
+
+
+function getparagraph7(){
+    var input=[];
+}
+for (var i=7; i<=12 ; ++i)
+{
+    inputs.push(document.getElementById("para7_input_box" + i).value);
+}
+inputs.join(" .")
+document.getElementById("showpragraph7").innerHTML=inputs.join(" .")
